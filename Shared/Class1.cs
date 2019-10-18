f8eec7d6efd88db929c78cbffd103cb8d49a0c29
woko <woko@bi-technologies.com.pl>
2019-10-18 12:08:45 +0200

    sfedrgtdertert

--- Shared/Class1.cs
+++ Shared/Class1.cs
@@ -6,12 +6,7 @@ namespace Shared
     {
         public Class1()
         {
-            var x = 4;
-            var y = 1;
-            var z = 1;
-            var a = 2;
-            var b = 2;
-            var c = 2;
+
         }
     }
 }
--- Shared/Class3.cs
+++ Shared/Class3.cs
@@ -8,7 +8,12 @@ namespace Shared
     {
         public Class3()
         {
-
+            var x = 4;
+            var y = 1;
+            var z = 1;
+            var a = 2;
+            var b = 2;
+            var c = 2;
         }
     }
 }
